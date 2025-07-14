3ae4fd8a20b7c10a3aa55c0a12551ca801b79d78
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-07-14 16:15:49 +0700

    files

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -2,7 +2,7 @@ import torch
 import torchaudio
 import numpy as np
 
-class AudioPreprocessor:
+class Preprocessor:
     def __init__(self, fft_bins, hop_len, sample_rate):
         self.fft_bins = fft_bins
         self.hop_len = hop_len
@@ -10,4 +10,3 @@ class AudioPreprocessor:
         self.windows = torch.hann_window(fft_bins)
 
     def to_spectrogram(self, waveform):
-        
