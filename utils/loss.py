849d25082d27883639053a5e34d799c533c128c6
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-07-24 18:09:42 +0700

    loss func

--- utils/loss.py
+++ utils/loss.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 
 class TimeFreqDomainLoss(nn.Module):
     def __init__(self, alpha=0.5):
-        super(MultiDomainLoss, self).__init__()
+        super(TimeFreqDomainLoss, self).__init__()
         self.l1 = nn.L1Loss()
         self.alpha = alpha
+
+    def forward(self, pred_wave, pred_spec, target_wave, target_spec):
+        time_loss = self.l1(pred_wave, target_wave)
+        freq_loss = self.l1(pred_spec.abs, target_spec.abs())
+        return self.alpha*time_loss+(1-self.alpha)*freq_loss
